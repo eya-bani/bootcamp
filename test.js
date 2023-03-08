aa50b3a3a2dd81248b11bc50f27fad45ac32cfcf
eya-bani <bani.eya@intact.fr>
2023-03-08 11:30:17 +0100

    boot

---
+++ test.js
@@ -0,0 +1,13 @@
+let a = 5;
+switch (a){
+    case 0 :
+    console.log("monday");
+    break;
+        
+    case 1 :
+        console.log("mond");
+        break;
+        default :
+            console.log("nooo");
+            break;
+}
